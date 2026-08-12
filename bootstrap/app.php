58217c0181c2db0013a632146d569d80c8ea276e
KleitonFerreira <141092094+kleitonfr@users.noreply.github.com>
2026-08-11 22:50:26 -0300

    Fix HTTPS proxy detection for Render

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,9 +9,9 @@ return Application::configure(basePath: dirname(__DIR__))
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
     )
-    ->withMiddleware(function (Middleware $middleware) {
-        //
+    ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
     })
-    ->withExceptions(function (Exceptions $exceptions) {
+    ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
